a31b5c3cccb6bda8bd1895685301a6204edbf914
nicocortese <nicolascortese2004@gmail.com>
2025-08-27 15:09:17 -0300

    navbar1

---
+++ src/components/HomeContainer.js
@@ -0,0 +1,7 @@
+const HomeContainer = () => {
+  return (
+    <h1 className="flex justify-center items-center pt-20 text-7xl font-bold">ROPA RETRO</h1>
+  )
+}
+
+export default HomeContainer
---
+++ src/components/ShopGrid.js
@@ -0,0 +1,9 @@
+import ShopCards from "./ShopCards"
+
+const ShopGrid = () => {
+  return (
+    <div>ShopGrid</div>
+  )
+}
+
+export default ShopGrid
