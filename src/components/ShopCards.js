b4b840f2e8662601cc078c7fbfff61acfb2471f0
nicocortese <nicolascortese2004@gmail.com>
2025-08-28 18:38:00 -0300

    cambios en grid

--- src/components/ShopCards.js
+++ src/components/ShopCards.js
@@ -1,19 +1,19 @@
 import Image from "next/image";
-const ShopCards = () => {
+
+const ShopCards = ({ product }) => {
   return (
     <article className="col-span-1 rounded-3xl overflow-hidden bg-[#FFEFEF] shadow-lg">
       <div className="relative w-full h-[300px]">
         <Image
-          src="/assets/imgs/producto1.jpg"
-          alt="Producto 1"
+          src={product.image}
+          alt={product.name}
           fill
           className="rounded-3xl object-cover"
         />
-      
 
-      <div className="absolute inset-0 flex items-center justify-center font-bold text-xl z-10">
-        <h3>Converse rojas y feas</h3>
-      </div>
+        <div className="absolute inset-0 flex items-center justify-center font-bold text-xl z-10 bg-black/40 text-white">
+          <h3>{product.name}</h3>
+        </div>
       </div>
     </article>
   );
