94ca6b716707140ce21d5b5ea59b333421078904
nicocortese <nicolascortese2004@gmail.com>
2025-09-25 19:49:41 -0300

    primeras tocadas de context

--- src/components/ShopGrid.js
+++ src/components/ShopGrid.js
@@ -1,14 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
 import Link from "next/link";
 import ShopCards from "./ShopCards";
+import { useAppContext } from "@/ShopContext";
 
 const ShopGrid = () => {
-  // Data temporal (después reemplazar con API)
-  const products = [
-    { id: 1, name: "Converse Chuck Taylor", image: "/assets/imgs/producto1.jpg", price: 180, discount: 20 },
-    { id: 2, name: "Adidas Superstar", image: "/assets/imgs/producto2.jpg", price: 150, discount: 10 },
-    { id: 3, name: "Puma Suede Classic", image: "/assets/imgs/producto3.jpg", price: 110 },
-    { id: 4, name: "Vans Old Skool", image: "/assets/imgs/producto4.jpg", price: 95, discount: 5 },
-  ];
+  const { products, getProducts, loading } = useAppContext();
+
+ useEffect(() => {
+  getProducts();
+ }, [getProducts]);
 
   return (
     <section className="max-w-[1200px] mx-auto px-4 py-10">
@@ -29,12 +31,14 @@ const ShopGrid = () => {
 
       {/* Grid de productos */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {products.map((product) => (
-          <ShopCards key={product.id} product={product} />
-        ))}
+      {loading ? (
+        <p>Cargando productos...</p>
+      ) : (
+        products.map((product) => <ShopCards key={product.id} product={product} />)
+      )}
       </div>
     </section>
   );
-};
+}
 
 export default ShopGrid;
